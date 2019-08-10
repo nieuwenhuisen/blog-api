84b3d098d9a289997c9dd3092facb3a3610815a0
Jeroen van den Nieuwenhuisen <nieuwenhuisen@me.com>
2019-08-10 11:28:02 +0200

    Add category entity validation and groups

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,9 +6,14 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     normalizationContext={"groups"={"category:read"}},
+ *     denormalizationContext={"groups"={"category:write"}}
+ * )
  * @ORM\Entity(repositoryClass="App\Repository\CategoryRepository")
  */
 class Category
@@ -22,6 +27,8 @@ class Category
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"category:read", "category:write"})
+     * @Assert\NotBlank()
      */
     private $name;
 
