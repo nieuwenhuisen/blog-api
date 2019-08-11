722aed125686b8a6a07af268592748a5dff7715b
Jeroen van den Nieuwenhuisen <nieuwenhuisen@me.com>
2019-08-11 20:55:41 +0200

    Post validation and groups

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -6,10 +6,17 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
- * @ApiResource()
+ * @ApiResource(
+ *     normalizationContext={"groups"={"post:read"}},
+ *     denormalizationContext={"groups"={"post:write"}},
+ * )
  * @ORM\Entity(repositoryClass="App\Repository\PostRepository")
+ * @UniqueEntity(fields="slug")
  */
 class Post
 {
@@ -22,26 +29,35 @@ class Post
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"post:read", "post:write"})
+     * @Assert\NotBlank()
      */
     private $title;
 
     /**
      * @ORM\Column(type="string", length=100, unique=true)
+     * @Groups({"post:read", "post:write"})
+     * @Assert\NotBlank()
      */
     private $slug;
 
     /**
      * @ORM\Column(type="datetime", nullable=true)
+     * @Groups({"post:read", "post:write"})
+     * @Assert\DateTime()
      */
     private $publicationDate;
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"post:read", "post:write"})
+     * @Assert\NotBlank()
      */
     private $content;
 
     /**
      * @ORM\ManyToMany(targetEntity="App\Entity\Category", inversedBy="posts")
+     * @Groups({"post:read", "post:write"})
      */
     private $categories;
 
